80f1f0c4c0e0046cd62182c44d189336fff7cd9f
Mirko Brombin <mirkobrombin@users.noreply.github.com>
2017-09-24 09:04:52 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,20 +2,20 @@
 '''
    Copyright 2017 Mirko Brombin (brombinmirko@gmail.com)
 
-   This file is part of Piffy.
+   This file is part of Football.
 
-    Piffy is free software: you can redistribute it and/or modify
+    Football is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Piffy is distributed in the hope that it will be useful,
+    Football is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Piffy.  If not, see <http://www.gnu.org/licenses/>.
+    along with Football.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import gi
